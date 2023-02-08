d80b300d2c22eb803bce012f656ff6a180f7e7e1
Nabil <hayounn.etd@gmail.com>
2023-02-08 22:17:23 +0100

    Add input field

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      'monsters': []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users =>  this.setState({ monsters: users})
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const filteredMonsters = this.state.monsters.filter((monster) => monster.name.toLowerCase() === event.target.value.toLowerCase());
+          this.setState({
+            ...this.state,
+            monsters: filteredMonsters
+          })
+        }} />
+        {
+          this.state.monsters.map((moster) => {
+            return <h1 key={moster.name}>{moster.name}</h1>
+          })
+        }
       </div>
     );
   }
